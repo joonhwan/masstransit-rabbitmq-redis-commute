aeea6369aff1cd8e57dd9ca1005ab7ebfd39edc1
이준환 <joonhwan.lee@mirero.co.kr>
2021-03-08 16:31:47 +0900

    Masstransit.Platform 을 실제로 돌려봤음.

---
+++ Sample.Platforms/Sample.Platform.Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Sample.Platform.Commands;
+
+namespace Sample.Platform.Commands
+{
+    public interface SampleCommand
+    {
+        Guid CommandId { get; }
+        string Command { get; } 
+    }
+}
+
+namespace Sample.Platform.Client
+{
+    
+    class Program
+    {
+        static async Task Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+                
+            });
+
+            await bus.StartAsync();
+
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("명령을 입력해 보세요. 그냥 엔터 치면 작업을 모두 중단합니다.");
+                        var line = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(line))
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("명령을 전송합니다 : '{0}'", line);
+
+                        
+                        var message = new
+                        {
+                            CommandId = InVar.Id,
+                            Command = line
+                        };
+                        await bus.Publish<SampleCommand>(message);
+                    }
+                });
+            }
+            finally
+            {
+                await bus.StopAsync();
+            }
+        }
+    }
+}
